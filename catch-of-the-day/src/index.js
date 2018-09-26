e5b26f18a9966f6fc856d072c35dd34b0fdccc8a
Richard Lock <lock.j.h@gmail.com>
2018-09-26 08:42:42 +0800

    - added serviceWorker to index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import Router from './components/Router';
 import './css/style.css';
+import registerServiceWorker from './registerServiceWorker';
 
 render(<Router />, document.querySelector('#main'));
+registerServiceWorker();
